6bf880d82a9d83ce24bdfff098d20890e2d1609f
Gaurav-Jethuri <105909298+EndGaurav@users.noreply.github.com>
2023-03-23 16:06:53 +0530

    Update prod.py

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -3,7 +3,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS += ['http://domain.com']
+ALLOWED_HOSTS += ['http://domain.com', '50.16.120.226']
 WSGI_APPLICATION = 'home.wsgi.prod.application'
 
 DATABASES = {
